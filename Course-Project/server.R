438365f4eb657cdf82a278d7f24a73f9381b694f
Gaurav Kumar <gaurav.kumar@me.com>
2015-03-10 20:51:30 -0700

    next commit

--- Course-Project/server.R
+++ Course-Project/server.R
@@ -8,35 +8,5 @@ shinyServer(
   function(input, output) {
     output$fromCity2 <- renderPrint({input$fromCity1})
     output$toCity2 <- renderPrint({input$toCity1})
-#    text(output$fromCity)
-#     #output$oid2 <- renderPrint({input$id2})
-#     #output$odate <- renderPrint({input$date})
-#     #output$odate <- renderPrint({input$id3})
-#     output$inputValue <- renderPrint({input$glucose})
-#     output$prediction <- renderPrint({diabetesRisk(input$glucose)})
-#     output$newHist <- renderPlot({
-#       hist(galton$child, xlab="Child Height", col="blue", main="Histogram")
-#       mu <- input$mu
-#       lines(c(mu, mu), c(0,200), col="red", lwd=5)
-#       mse <- mean((galton$child - mu) ^ 2)
-#       text(63, 150, paste("mu =", mu))
-#       text(63, 140, paste("MSE =", round(mse, 2)))
+    output$distance <- renderPrint({eurodist[input$fromCity1, input$toCity1]})
     })
-
-# library(UsingR)
-# data(galton)
-# data(eurodist)
-# eurodist <- as.matrix(eurodist)
-# shinyServer(
-#   function(input, output) {
-#     output$myHist <- renderPlot({
-#       hist(galton$child, xlab='child height', col='lightblue',main='Histogram')
-#       mu <- input$mu
-#       lines(c(mu, mu), c(0, 200),col="red",lwd=5)
-#       mse <- mean((galton$child - mu)^2)
-#       text(63, 150, paste("mu = ", mu))
-#       text(63, 140, paste("MSE = ", round(mse, 2)))
-#     })
-#     
-#   }
-#)
