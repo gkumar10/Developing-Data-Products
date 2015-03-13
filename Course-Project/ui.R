670b7d07a488e0edc2f9e333cb94e7b37d9913b8
Gaurav Kumar <gaurav.kumar@me.com>
2015-03-12 22:21:24 -0700

    ..

--- Course-Project/ui.R
+++ Course-Project/ui.R
@@ -6,16 +6,21 @@ library(shiny)
 #convert eurodist to a matrix object. 
 eurodist <- as.matrix(eurodist)
 
+#set text for instructions
+text1 <- "Pick a city name from each of the two dropdown lists below."
+text2 <- "Click Submit button."
+
 shinyUI(
   pageWithSidebar(
     
     #set header text
-    headerPanel("Distance Between 2 European Cities"),
+    headerPanel("Find Distance Between 2 European Cities"),
   
     #fill side panel with help text (instructions), 2 list boxes and 1 submit button
     sidebarPanel(
       h1("Input:"),
-      helpText(strong("Instructions: Pick a city name from each of the two dropdown lists below. Click Submit button to learn the road distance (in km) between the 2 cities in Europe. ")),
+      helpText(strong("Instructions:")),
+      helpText(strong(tags$ol(tags$li(text1), tags$li(text2)))),
       selectInput("fromCity1", label="From:", choices=rownames(eurodist), selected=rownames(eurodist)[1]),
       selectInput("toCity1", label="To:", choices=colnames(eurodist), selected=colnames(eurodist)[2]),
       submitButton('Submit')
