d4d8a60f4cb7a3ee20763a738a586ab3ba2faef4
Gaurav Kumar <gaurav.kumar@me.com>
2015-03-12 21:42:26 -0700

    ..

--- Course-Project/server.R
+++ Course-Project/server.R
@@ -2,11 +2,25 @@ library(shiny)
 library(stats)
 data(eurodist)
 
+#The eurodist dataset give the road distances (in km) between 21 cities in Europe. The data are taken from a table in The Cambridge Encyclopaedia.
+
+#convert eurodist to a matrix object. 
 eurodist <- as.matrix(eurodist)
 
 shinyServer(
+  
   function(input, output) {
-    output$fromCity2 <- renderPrint({input$fromCity1})
-    output$toCity2 <- renderPrint({input$toCity1})
-    output$distance <- renderPrint({eurodist[input$fromCity1, input$toCity1]})
+    
+    #define output text
+    output$distancetext <- renderUI({
+      
+      #use HTML tags so city names and distances are bold
+      HTML('Distance between <strong>',
+            input$fromCity1,
+            "</strong> and <strong>",
+            input$toCity1,
+            "</strong> is <strong>",
+            eurodist[input$fromCity1, input$toCity1],
+            "kilometers</strong>.")
+   })
     })
--- Course-Project/ui.R
+++ Course-Project/ui.R
@@ -1,25 +1,30 @@
-setwd("~/Coursera/Developing-Data-Products/Course-Project")
+#setwd("~/Coursera/Developing-Data-Products/Course-Project")
 library(shiny)
 
+#The eurodist dataset give the road distances (in km) between 21 cities in Europe. The data are taken from a table in The Cambridge Encyclopaedia.
+
+#convert eurodist to a matrix object. 
 eurodist <- as.matrix(eurodist)
 
 shinyUI(
   pageWithSidebar(
+    
+    #set header text
     headerPanel("Distance Between 2 European Cities"),
   
+    #fill side panel with help text (instructions), 2 list boxes and 1 submit button
     sidebarPanel(
-      helpText(strong("Instructions: Pick a city name from each of the two dropdown lists below. Click Submit button to return the road distance (in km) between the 2 cities in Europe. ")),
+      helpText(strong("Instructions: Pick a city name from each of the two dropdown lists below. Click Submit button to learn the road distance (in km) between the 2 cities in Europe. ")),
       selectInput("fromCity1", label="From:", choices=rownames(eurodist), selected=rownames(eurodist)[1]),
       selectInput("toCity1", label="To:", choices=colnames(eurodist), selected=colnames(eurodist)[2]),
       submitButton('Submit')
       ),
+    
     mainPanel(
-      p("Distance between ",
-            verbatimTextOutput("fromCity2"),
-            " and ",
-            verbatimTextOutput("toCity2"),
-            " is "),
-      verbatimTextOutput("distance")
+      
+      #show output with HTML tags
+      uiOutput("distancetext")
+      
     )
   )
 )
