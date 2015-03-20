b2e4ef2be6a01a0cd162f28a102c4c298f4c7e88
Gaurav Kumar <gkumar10@gmail.com>
2015-03-19 22:45:39 -0700

    mostly final commit for shiny files

--- Course-Project/ui.R
+++ Course-Project/ui.R
@@ -1,4 +1,3 @@
-setwd("~/Coursera/Developing-Data-Products/Course-Project")
 library(shiny)
 
 #The eurodist dataset give the road distances (in km) between 21 cities in Europe. The data are taken from a table in The Cambridge Encyclopaedia.
@@ -18,9 +17,10 @@ shinyUI(
   
     #fill side panel with help text (instructions), 2 list boxes and 1 submit button
     sidebarPanel(
-      h1("Input:"),
+      h4("This Shiny app will provide road distances in kilometers between 21 European cities."),
       helpText(strong("Instructions:")),
       helpText(strong(tags$ol(tags$li(text1), tags$li(text2)))),
+      h2("Input:"),
       selectInput("fromCity1", label="From:", choices=rownames(eurodist), selected=rownames(eurodist)[1]),
       selectInput("toCity1", label="To:", choices=colnames(eurodist), selected=colnames(eurodist)[2]),
       submitButton('Submit')
@@ -29,7 +29,7 @@ shinyUI(
     mainPanel(
       
       #show output
-      h1("Output:"),
+      h2("Output:"),
       uiOutput("distancetext")
       
     )
